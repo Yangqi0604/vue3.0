485412672751b8df616c45ebb258829c224148ff
Aaron <1132413764@qq.com>
2020-03-20 22:22:53 +0800

    bugfix

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(VueRouter);
+Vue.use(ViewUI);
+
 
 Vue.config.productionTip = false
 
